d196a655ac6cb3fdae1319f1f35a8f3ac35af220
Jean Paul Saucedo <jpscorp21@gmail.com>
2018-11-26 13:05:06 -0300

    Proyecto inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/page-routing.module.ts
@@ -0,0 +1,15 @@
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+    {
+
+    }
+]
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+})
+export class PageRoutingModule {
+    
+}
